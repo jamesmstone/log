834088ef0a4f1dd7bf794b16cf5063bb54876dc1
James Stone <112-jstone@users.noreply.gitlab.tradition-private>
2020-02-02 11:36:37 +1100

    add git test command

--- api/test.js
+++ api/test.js
@@ -1,7 +1,25 @@
+const { execSync } = require('child_process');
+
+function run() {
+
+   return  execSync('git status', (err, stdout, stderr) => {
+        if (err) {
+            // node couldn't execute the command
+            console.error(err)
+            return;
+        }
+
+        // the *entire* stdout and stderr (buffered)
+        console.log(`stdout: ${stdout}`);
+        console.log(`stderr: ${stderr}`);
+    });
+
+}
 module.exports = (req, res) => {
     res.json({
         body: req.body,
         query: req.query,
-        cookies: req.cookies
+        cookies: req.cookies,
+        command: run()
     })
 }
