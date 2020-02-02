cd15d560a652f68152c7c94ca3d5acff2a70c1fd
James Stone <112-jstone@users.noreply.gitlab.tradition-private>
2020-02-02 15:11:45 +1100

    update location

--- api/location.js
+++ api/location.js
@@ -1,17 +1,32 @@
 import {updateFile} from "./_updateFile";
 
 module.exports = (req, response) => {
-    updateFile('data/locations.json', (oldContent => {
-        let json = JSON.parse(oldContent);
-        json.locations.push({...JSON.parse(req.body)});
-        return JSON.stringify(json)
-    })).then(() => {
-        response.json({success: true})
-    }).catch((error) => {
-        response.status(400);
-        response.json({success: false, error: error})
+    try {
+        let data = []
+        req.on('data', chunk => {
+            data.push(chunk)
+        })
+        req.on('end', () => {
+            const body = JSON.parse(data)
+            updateFile('data/locations.json', (oldContent => {
+                let json = JSON.parse(oldContent);
+                json.locations.push(body);
+                return JSON.stringify(json)
+            })).then(() => {
+                response.json({success: true})
+            }).catch((error) => {
+                response.status(400);
+                response.json({success: false, error: error})
 
-    })
+            })
+        })
+
+
+    } catch (e) {
+        console.error(e, req)
+        response.status(400);
+        response.json({success: false, error: e})
+    }
 
 
 };
