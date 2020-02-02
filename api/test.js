f85fc5bb6e3cfccc9296d676ba79e9240c2a611d
James Stone <112-jstone@users.noreply.gitlab.tradition-private>
2020-02-02 12:46:19 +1100

    add git test command

--- api/test.js
+++ api/test.js
@@ -4,32 +4,10 @@ const Octokit = require("@octokit/rest"),
 
 module.exports = (req, res) => {
 
-    const event = req;
 
     try {
         const octokit = new Octokit({auth: process.env['github-token']});
-        if (!event.body) {
-            return {
-                statusCode: 500,
-                body: 'Title and link are required.'
-            };
-        }
-        const body = JSON.parse(event.body);
-        const newItem = {};
-        newItem.title = body.title;
-        newItem.link = body.link;
-        if (!newItem.title) {
-            return {
-                statusCode: 500,
-                body: 'title parameter required'
-            };
-        }
-        if (!newItem.link) {
-            return {
-                statusCode: 500,
-                body: 'link parameter required'
-            };
-        }
+
         let path = 'data/requests.json';
         return octokit.repos.getContents({
             owner,
@@ -43,7 +21,7 @@ module.exports = (req, res) => {
             let message = 'Updated requests';
             let content = '';
             let sha = res.data.sha;
-            requestsJson.requests.push(newItem);
+            requestsJson.requests.push(req);
             requestsRaw = JSON.stringify(requestsJson);
             buff = Buffer.from(requestsRaw);
             content = buff.toString('base64');
