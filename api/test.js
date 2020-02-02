28b7978844b66d3911f4124b64670421bb776f7f
James Stone <112-jstone@users.noreply.gitlab.tradition-private>
2020-02-02 13:29:13 +1100

    update git ignore

--- api/test.js
+++ api/test.js
@@ -2,26 +2,25 @@ const Octokit = require("@octokit/rest"),
     owner = 'jamesmstone',
     repo = 'log';
 
-module.exports = (req, res) => {
+module.exports = (req, response) => {
 
 
     try {
-        const octokit = new Octokit({auth: process.env['github-token']});
+        const octokit = new Octokit({auth: process.env['GITHUB_TOKEN']});
 
         let path = 'data/requests.json';
         return octokit.repos.getContents({
             owner,
             repo,
             path
-        }).then(res => {
-            console.log(res);
-            let buff = Buffer.from(res.data.content, 'base64');
+        }).then(result => {
+            let buff = Buffer.from(result.data.content, 'base64');
             let requestsRaw = buff.toString('utf-8');
             let requestsJson = JSON.parse(requestsRaw);
             let message = 'Updated requests';
             let content = '';
-            let sha = res.data.sha;
-            requestsJson.requests.push(req);
+            let sha = result.data.sha;
+            requestsJson.requests.push({body: req.body, query: req.query});
             requestsRaw = JSON.stringify(requestsJson);
             buff = Buffer.from(requestsRaw);
             content = buff.toString('base64');
@@ -32,17 +31,17 @@ module.exports = (req, res) => {
                 message,
                 content,
                 sha
-            }).then(res => {
-                return {
+            }).then(result => {
+                response.json({
                     statusCode: 200,
                     body: '{"success":"true"}'
-                }
-            })
-        });
+                })
+            }).catch(console.error)
+        }).catch(console.error);
 
 
     } catch (err) {
-        return {statusCode: 500, body: err.toString()};
+        response.json({statusCode: 500, body: err.toString()});
     }
 
 };
